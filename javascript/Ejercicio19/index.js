317403b065f00faaa03f690f9f297705bb33cbd1
hristochavez <hachavez@protonmail.com>
2025-09-28 22:28:13 -0500

    Ejercicio 19: Ejercicio para aprender a usar fetch.

--- javascript/Ejercicio19/index.js
+++ javascript/Ejercicio19/index.js
@@ -1,4 +1,5 @@
-// Ejercicio 16-18.
+// Ejercicio 19.
+
 const students = [];
 
 // Tabla.
@@ -12,7 +13,9 @@ const addStudentBtn = document.getElementById('addStudent');
 
 // Valores para nuevo estudiante.
 const stuNameInput = document.getElementById('stuName');
+const stuEmailInput = document.getElementById('stuEmail');
 const stuNoteInput = document.getElementById('stuNote');
+const stuPhoneInput = document.getElementById('stuPhone');
 
 // Inputs de estudiante a editar.
 const stuUpdateNameInput = document.getElementById('stuUpdateName');
@@ -41,23 +44,13 @@ window.addEventListener('click', (e) => {
     }
 });
 
-// Actualiza a un estudiante de la tabla y del array.
-const openModalUpdateStudent = (e) => {
-    if (e.target.classList.contains('update')) {
-        const row = e.target.closest('tr');
-        const id = modalContent.dataset.id = parseInt(row.dataset.id);
 
-        // Busca al estudiante y llena su información en el modal
-        const student = students.find(student => student.id === id);
-        stuUpdateNameInput.value = student.name;
-        stuUpdateNoteInput.value = student.note;
-
-        // Apertura el modal.
-        modal.style.display = 'flex';
-    }
-};
-
-// Agrega el botón para borrar estudiante.
+/**
+ * Agrega los botones de borrar y actualizar para un estudiante.
+ *  
+ * @param {*} row - Fila donde se agregaran los botones.
+ * @returns {void}
+ */
 const addButtons = (row) => {
     const cell = document.createElement('td');
 
@@ -75,19 +68,25 @@ const addButtons = (row) => {
     row.appendChild(cell);
 };
 
-// Agregar estudiantes a la tabla
+/**
+ * Agrega un nuevo estudiante a la tabla y al array.
+ * 
+ * @returns {void}
+ */
 const addStudent = () => {
     // Obtiene los valores del pequeño formulario.
     const stuName = stuNameInput.value.trim();
     const stuNote = Number(stuNoteInput.value);
+    const stuEmail = stuEmailInput.value.trim();
+    const stuPhone = stuPhoneInput.value
 
     if (!stuName) {
-        alert('Ingrese un nombre válido');
+        alert('Ingrese un nombre válido.');
         return;
     }
 
     if (isNaN(stuNote)) {
-        alert('Ingrese una nota válida')
+        alert('Ingrese una nota válida.')
         return;
     }
 
@@ -96,23 +95,42 @@ const addStudent = () => {
         return;
     }
 
+    if (!stuEmail) {
+        alert('Ingrese un email válido.');
+        return;
+    }
+
+    if (isNaN(stuPhone)) {
+        alert('Ingrese un teléfono válido.');
+        return;
+    }
+
     // Id de nuevo estudiante.
     const newId = students.length === 0 ? 1 : students[students.length - 1].id + 1;
 
-    // Agrega el nuevo estudiante y su nota al arreglo.
-    students.push({ id: newId, name: stuName, note: stuNote });
+    // Agrega el nuevo estudiante al arreglo.
+    students.push({ id: newId, name: stuName, note: stuNote, email: stuEmail, phone: stuPhone });
 
-    // Crea una nueva fila e inserta los datos en ella.
+    // Crea una nueva fila.
     const row = document.createElement('tr');
     row.dataset.id = newId;
+
     const cellName = document.createElement('td');
+    cellName.dataset.name = cellName.textContent = stuName;
+
     const cellNote = document.createElement('td');
+    cellNote.dataset.note = cellNote.textContent = stuNote;
 
-    cellName.textContent = stuName;
-    cellNote.textContent = stuNote;
+    const cellEmail = document.createElement('td');
+    cellEmail.dataset.email = cellEmail.textContent = stuEmail
 
-    // Agrega una nueva fila a la tabla.
+    const cellPhone = document.createElement('td');
+    cellPhone.dataset.phone = cellPhone.textContent = stuPhone
+
+    // Agrega la nueva fila a la tabla.
     row.appendChild(cellName);
+    row.appendChild(cellEmail);
+    row.appendChild(cellPhone);
     row.appendChild(cellNote);
 
     // Botón para borrar y actualizar estudiante.
@@ -120,7 +138,30 @@ const addStudent = () => {
     stuTableBody.appendChild(row);
 };
 
-// Actualiza la información de un estudiante.
+/**
+ * Muenstra el modal para actualizar un estudiante. 
+ * @param {*} e - Evento click.
+ * @returns {void}
+ */
+const openModalUpdateStudent = (e) => {
+    if (e.target.classList.contains('update')) {
+        const row = e.target.closest('tr');
+        modalContent.dataset.id = parseInt(row.dataset.id);
+
+        // Busca al estudiante y llena su información en el modal
+        stuUpdateNameInput.value = row.querySelector('td[data-name]').dataset.name;
+        stuUpdateNoteInput.value = row.querySelector('td[data-note]').dataset.note;
+
+        // Apertura el modal.
+        modal.style.display = 'flex';
+    }
+};
+
+/**
+ * Actualiza la información de un estudiante.
+ * 
+ * @returns {void}
+ */
 const updateStudent = () => {
     // Nuevos valores del estudiante.
     const newName = stuUpdateNameInput.value.trim();
@@ -144,13 +185,37 @@ const updateStudent = () => {
     // Traemos la informacion del estudiante a editar.
     const student = students.find(student => student.id === parseInt(modalContent.dataset.id));
 
+    // Guardamos la nueva información.
     student.name = newName;
     student.note = newNote;
+
+    updateRow(student);
 
     modal.style.display = 'none';
 };
 
-// Borra un estudiante de la tabla y del array.
+/**
+ * Actualiza la información del estudiante en la tabla.
+ * 
+ * @param {*} student 
+ * @returns {void}
+ */
+const updateRow = (student) => {
+    const row = document.querySelector(`tr[data-id="${student.id}"]`);
+
+    const cellName = row.querySelector('td[data-name]');
+    cellName.dataset.name = cellName.textContent = student.name;
+
+    const cellNote = row.querySelector('td[data-note]');
+    cellNote.dataset.note = cellNote.textContent = student.note;
+};
+
+/**
+ * Borra a un estdiante de la tabla y del array.
+ * 
+ * @param {MouseEvent} e - Evento click
+ * @returns {void}
+ */
 const deleteStudent = (e) => {
     // Se identifica que se presionó un botón en el cuerpo de la tabla.
     if (e.target.classList.contains('delete')) {
@@ -168,33 +233,47 @@ const deleteStudent = (e) => {
     }
 };
 
-// Inserción de datos en la tabla.
+/**
+ * Llena la tabla con información de los estudiantes.
+ * 
+ * @returns {void}
+ */
 const populateTable = async () => {
-    // Limpia todo el cuerpo de la tabla antes de insertar las filas.
-    stuTableBody.replaceChildren();
+    try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/users');
+        const users = await response.json();
 
-    const response = await fetch('https://jsonplaceholder.typicode.com/users');
-    const users = await response.json();
+        users.forEach(user => {
+            // Crea una nueva fila e inserta los datos en ella.
+            const row = document.createElement('tr');
 
-    users.forEach(user => {
-        // Crea una nueva fila e inserta los datos en ella.
-        const row = document.createElement('tr');
-        row.dataset.id = user.id;
+            // Datos que solo necesitamos del JSON.
+            row.dataset.id = user.id;
+            ['name', 'email', 'phone'].forEach(key => {
+                const cell = document.createElement('td');
+                cell.textContent = user[key];
+                cell.dataset[key] = user[key];
+                row.appendChild(cell);
+            });
 
-        // Nota aleatoria entre 0 y 20 (incluyendo).
+            // Nota aleatoria entre 0 y 20 (incluyendo).
+            const note = Math.floor(Math.random() * (20 + 1)) + 0;
+            const noteCell = document.createElement('td');
+            noteCell.textContent = note;
+            noteCell.dataset.note = note
+            row.appendChild(noteCell);
 
-        ['name', 'email', 'phone'].forEach(key => {
-            const cell = document.createElement('td');
-            cell.textContent = user[key];
-            row.appendChild(cell);
+            // Agregamos el nuevo estudiante al array.
+            students.push({ id: user.id, name: user.name, email: user.email, phone: user.phone, note: note });
+
+            addButtons(row);
+
+            // Agrega una nueva fila a la tabla.
+            stuTableBody.appendChild(row);
         });
-
-        // Botón para borrar y actualizar estudiante.
-        addButtons(row);
-
-        // Agrega una nueva fila a la tabla.
-        stuTableBody.appendChild(row);
-    });
+    } catch (error) {
+        console.error(error);
+    }
 };
 
 addStudentBtn.addEventListener('click', addStudent);
